cef02e346e2d8bdceb82445cd037e724a66fb49f
rahul mahato <rahulmahato0852@gmail.com>
2024-02-09 18:58:36 +0530

    add frontend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 require("dotenv").config({ path: "./.env" })
-
+const path = require('path')
 
 mongoose.connect(process.env.MONGO_URL)
 
@@ -12,12 +12,16 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname, "dist")))
 
 app.use("/api/admin", require("./routes/todoRoute"))
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+})
 
 mongoose.connection.once("open", () => {
     console.log("Mongoose connected");
-    app.listen(5000, console.log("Server running"))
+    app.listen(process.env.PORT, console.log("Server running"))
 })
 
 
